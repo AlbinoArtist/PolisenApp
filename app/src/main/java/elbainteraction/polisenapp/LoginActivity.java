b5feb7bb9a30fd69fdc378fae93392b2f2801b5b
AlbinoArtist <henrikpaoledlund@hotmail.com>
2015-10-28 11:50:50 +0100

    Addded avtivity for logging into the system. Small buggfixes.

---
+++ app/src/main/java/elbainteraction/polisenapp/LoginActivity.java
@@ -0,0 +1,36 @@
+package elbainteraction.polisenapp;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+
+import com.ogaclejapan.smarttablayout.SmartTabLayout;
+import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItemAdapter;
+import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItems;
+
+public class LoginActivity extends AppCompatActivity {
+    FragmentPagerItemAdapter adapter;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        adapter = new FragmentPagerItemAdapter(
+                getSupportFragmentManager(), FragmentPagerItems.with(this)
+                .add("Page 1", TestFragment.class)
+                .add("Page 2", TestFragment.class)
+                .create());
+
+        ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
+        viewPager.setAdapter(adapter);
+
+        SmartTabLayout viewPagerTab = (SmartTabLayout) findViewById(R.id.viewpagertab);
+        viewPagerTab.setViewPager(viewPager);
+    }
+    public void onPageSelected(int position) {
+
+        //.instantiateItem() from until .destoryItem() is called it will be able to get the Fragment of page.
+
+        Fragment page = adapter.getPage(position);
+    }
+}
