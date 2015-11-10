c9184128124670d58ccf7dfe8c8eea87ec479060
fbontin <fbontin@gmail.com>
2015-11-10 16:46:17 +0100

    Add Culprit.java and connect with xml.

---
+++ app/src/main/java/elbainteraction/polisenapp/AnmalanPackage/EditReportPackage/Culprit.java
@@ -0,0 +1,102 @@
+package elbainteraction.polisenapp.AnmalanPackage.EditReportPackage;
+
+/**
+ * Created by fbont on 2015-11-10.
+ */
+public class Culprit {
+
+    private boolean known;
+    private String firstName, surName, personNumber, sex;
+
+    //known
+    private String phoneNumber, email;
+
+    //unknown
+    private int length, age;
+    private String description;
+
+    /*@known Set to true if culprit is known, otherwise false (radiobuttons in xml).*/
+    public Culprit(boolean known){
+        this.known = known;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getSurName() {
+        return surName;
+    }
+
+    public void setSurName(String surName) {
+        this.surName = surName;
+    }
+
+    public String getPersonNumber() {
+        return personNumber;
+    }
+
+    public void setPersonNumber(String personNumber) {
+        this.personNumber = personNumber;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isKnown() {
+        return known;
+    }
+
+    public void setKnown(boolean known) {
+        this.known = known;
+    }
+}
